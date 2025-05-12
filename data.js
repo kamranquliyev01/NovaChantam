ac63c1573f07d143f9ff21151b4549e419ab60ca
kamranquliyev01 <kamranquliyev01@gmail.com>
2025-05-12 22:43:26 +0400

    Delete data.js

--- data.js
+++
@@ -1,6 +0,0 @@
-const data = [
-  { Kod: 1, Qiymət: "", Açıqlama: "Ölçü 16sm" },
-  { Kod: 2, Qiymət: "95 AZN", Açıqlama: "" },
-  { Kod: 3, Qiymət: "80 - 95 AZN", Açıqlama: "Ölçü 17sm və 24sm" },
-  // Bura digər kodları əlavə edə bilərsən...
-];
